d2d1c6e50da632bcc1f86c7e015fbacb2d055df6
nirtzur <nirtzur89@gmail.com>
2019-01-19 15:29:50 +0100

    test

--- routes/signup_pers.js
+++ routes/signup_pers.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router  = express.Router();
 
 /* GET personal Signup page */
-router.get('/', (req, res, next) => {
+router.get('/signup_pers', (req, res, next) => {
+  console.log('blablablablabla')
   res.send('Personal Signup Page');
 });
 
