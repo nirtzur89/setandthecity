5d84c538afb840d6a45795e7a4c811f1a3164a3e
Katharina Koenig <k.koenig1811@gmail.com>
2019-01-19 15:53:56 +0100

    whatever random comment

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 // default value for title local
 app.locals.title = 'Set And The CITY!!! ';
 
-
+// random comment
 
 const index = require('./routes/index');
 app.use('/', index);
