96a5408f7c4a34b75d11e2f84765d01dbf031a8c
nirtzur <nirtzur89@gmail.com>
2019-01-24 17:23:56 +0100

    user name shows on hbs

--- routes/personal.js
+++ routes/personal.js
@@ -1,6 +1,19 @@
 const express = require('express');
+const hbs     = require('express-handlebars');
+
 const router  = express.Router();
 
+// //hbs properties
+// var userhbs = hbs.create({
+//   // Specify helpers which are only registered on this instance.
+//   helpers: {
+//       username: function () { return req.user.username; },
+//       bar: function () { return 'BAR!'; }
+//   }
+// });
+
+
+//checkin if user is authorized
 const authCheck = (req,res,next) => {
   if(!req.user){
     res.redirect('/')
@@ -9,10 +22,13 @@ const authCheck = (req,res,next) => {
   }
 };
 
+//route
 router.get('/', authCheck ,(req, res) => {
   //res.send("WE ARE AT YOUR PERSONAL 20 U 4 - WELCOME " + req.user.username);
-  res.render('personal')
+  var userName = req.user.username
   
+  
+  res.render('personal',{userName:userName})
 });
 
 module.exports = router;
