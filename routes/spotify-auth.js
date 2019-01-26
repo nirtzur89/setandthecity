b926338dd4ea509d0e0276cd066727e398d3e450
Katharina Koenig <k.koenig1811@gmail.com>
2019-01-26 13:10:48 +0100

    everything regarding the auth process is now in spotify-auth.js

--- routes/spotify-auth.js
+++ routes/spotify-auth.js
@@ -7,9 +7,7 @@ const User            = require('../models/user');
 const SpotifyWebApi = require('spotify-web-api-node');
 
 const client_id = 'a3676c8b791c49048c222a84f7fd770c';
-const client_id2 = 'daaf597517cb41bd84f0981584dbbc18';
 const client_secret = '54708907189e407e8c5f6eb2328f9374';
-const client_secret2 = 'd6fd4f5a4e3b40a685f4dde2a6058099';
 
 
 //mongoose
@@ -36,8 +34,8 @@ passport.deserializeUser((id, done)=>{
 //passport strategy
 passport.use(
   new SpotifyStrategy({
-       clientID: client_id2,
-       clientSecret: client_secret2,
+       clientID: client_id,
+       clientSecret: client_secret,
        callbackURL: '/logincon'
     },(accessToken, refreshToken, expires_in, profile, done) => {
       
@@ -64,4 +62,62 @@ passport.use(
         }})
       }));
 
-      module.exports = router;
+      var scopes = ['user-read-private', 'user-read-email'],
+  state = 'some-state-of-my-choice';
+
+
+
+const redirectUri = '/logincon';
+
+
+var spotifyApi = new SpotifyWebApi({
+  clientId: client_id,
+  clientSecret: client_secret,
+  redirectUri: redirectUri
+});
+
+
+// Create the authorization URL
+var authorizeURL = spotifyApi.createAuthorizeURL(scopes, state);
+
+// https://accounts.spotify.com:443/authorize?client_id=5fe01282e44241328a84e7c5cc169165&response_type=code&redirect_uri=https://example.com/callback&scope=user-read-private%20user-read-email&state=some-state-of-my-choice
+console.log(authorizeURL);
+
+var credentials = {
+  clientId: 'someClientId',
+  clientSecret: 'someClientSecret',
+  redirectUri: '/logincon'
+};
+
+var spotifyApi = new SpotifyWebApi(credentials);
+
+// Retrieve an access token and a refresh token
+spotifyApi.authorizationCodeGrant(authorizeURL).then(
+  function (data) {
+    console.log('The token expires in ' + data.body['expires_in']);
+    console.log('The access token is ' + data.body['access_token']);
+    console.log('The refresh token is ' + data.body['refresh_token']);
+
+    // Set the access token on the API object to use it in later calls
+    spotifyApi.setAccessToken(data.body['access_token']);
+    spotifyApi.setRefreshToken(data.body['refresh_token']);
+  },
+  function (err) {
+    console.log('Something went wrong!', err);
+  }
+);
+
+// clientId, clientSecret and refreshToken has been set on the api object previous to this call.
+spotifyApi.refreshAccessToken().then(
+  function (data) {
+    console.log('The access token has been refreshed!');
+
+    // Save the access token so that it's used in future calls
+    spotifyApi.setAccessToken(data.body['access_token']);
+  },
+  function (err) {
+    console.log('Could not refresh access token', err);
+  }
+);
+
+module.exports = router;
